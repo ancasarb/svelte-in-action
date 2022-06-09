ae0a6541d7aae853aed233da2556baf3991edb9b
asarb <asarb@expediagroup.com>
2022-06-09 12:31:55 +0100

    front end routing

---
+++ fullstack-svelte/school-lunch/frontend/src/views/admin/AdminLayout.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onMount } from 'svelte'
+  import { Route } from 'svelte-router-spa'
+  import { user } from '../../stores'
+
+  export let currentRoute
+  let initialized = false
+
+  onMount(() => {
+    user.set({
+      name: 'Test User',
+      schoolName: 'Test School',
+    })
+    initialized = true
+  })
+</script>
+
+<div>
+  {#if initialized}
+    <Route {currentRoute} />
+  {/if}
+</div>
---
+++ fullstack-svelte/school-lunch/frontend/src/views/admin/LunchMenuAdmin.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { user } from '../../stores'
+</script>
+
+<div>
+  <h1 class="title is-4">Lunch Menu Admin / {$user.schoolName}</h1>
+</div>
